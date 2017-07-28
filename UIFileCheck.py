6478b5c4490b9d7d1f16f610dca8f20718dd4dd3
Jennifer Goddard Buckley <jgbcodes@gmail.com>
2017-07-27 21:57:26 -0700

    UI File Check update
    updated version of Python UI File Check

--- UIFileCheck.py
+++ UIFileCheck.py
@@ -15,12 +15,16 @@ root =Tk()
 conn = sqlite3.connect('fileCheck.db')
 c=conn.cursor()
 c.execute('CREATE TABLE IF NOT EXISTS fileTimes(time TIMESTAMP)')
-c.execute('INSERT INTO fileTimes VALUES(0000)')
+c.execute('INSERT INTO fileTimes VALUES(2017)')
 
 
 ####################GUI##########################################
 
+
+
 def load_gui():
+
+
     source_fldr = ttk.Button(text = "Source Folder:", command = askSource).grid(row =0,
                             column =2, padx = 10, pady =10)
     pathLabel = ttk.Entry(text = sentF).grid(row =0, column=1, padx =10, pady =10)
@@ -29,7 +33,7 @@ def load_gui():
                                 column = 2, padx = 10, pady = 10)
     pathLabel = ttk.Entry(text = destF).grid(row = 1, column = 1, padx = 10, pady=10)
 
-    checkFile = ttk.Button(text = "Send Files:", command = addedFunc).grid(row = 2,
+    checkFile = ttk.Button(text = "Send Files:", command = fileCopy).grid(row = 2,
                            column = 2,  padx = 10, pady = 10)
     timeLabel = ttk.Label(text = "Last File Check:" + str(cTime)).grid(row = 2,
                           column = 1, padx = 10, pady = 10)
@@ -40,7 +44,6 @@ def load_gui():
 
 
 ##############FUNCTIONS###############
-
 
 sentF = StringVar()
 destF = StringVar()
@@ -53,10 +56,10 @@ def askDestination():
     desName = filedialog.askdirectory()
     destF.set(desName)
 
+
 def fileCopy():
     now = dt.datetime.now()
-    c.execute('INSERT INTO fileTimes VALUES(?)',
-              (now.strftime('%Y-%m-%d %H: %M'),))
+    c.execute('INSERT INTO fileTimes VALUES(?)',(now.strftime('%Y-%m-%d %H: %M'),))
     conn.commit()
 
  #get the paths for send and receive folders
@@ -70,16 +73,15 @@ def fileCopy():
         if mtime > beforeToday:
             shutil.copy2(path, receiving)
             print (newFiles)
-
-def fileTime():
+##def fileTime():
 #last file time
-   c.execute('SELECT time FROM fileTimes ORDER by time DESC')
-   cTime = c.fetchone()[0]
-
+c.execute('SELECT time FROM fileTimes ORDER by time DESC')
+cTime = c.fetchone()[0]
+   
             
-def addedFunc():
-    fileCopy()
-    fileTime()
+##def addedFunc():
+##    fileCopy()
+##    fileTime()
 
 
     
