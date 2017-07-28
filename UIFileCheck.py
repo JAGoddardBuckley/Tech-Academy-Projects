41e5ab0e59a6cd8d76ecd132b416fce437f95c5e
Jennifer Goddard Buckley <jgbcodes@gmail.com>
2017-07-27 19:12:50 -0700

    updated
    updated

--- UIFileCheck.py
+++ UIFileCheck.py
@@ -21,15 +21,15 @@ c.execute('INSERT INTO fileTimes VALUES(0000)')
 ####################GUI##########################################
 
 def load_gui():
-    source_fldr = ttk.Button(text = "Source Folder:", command = askSource()).grid(row =0,
+    source_fldr = ttk.Button(text = "Source Folder:", command = askSource).grid(row =0,
                             column =2, padx = 10, pady =10)
     pathLabel = ttk.Entry(text = sentF).grid(row =0, column=1, padx =10, pady =10)
 
-    destination_fldr = ttk.Button(text = "Destination Folder:", command = askDestination()).grid(row = 1,
+    destination_fldr = ttk.Button(text = "Destination Folder:", command = askDestination).grid(row = 1,
                                 column = 2, padx = 10, pady = 10)
     pathLabel = ttk.Entry(text = destF).grid(row = 1, column = 1, padx = 10, pady=10)
 
-    checkFile = ttk.Button(text = "Send Files:", command = fileCopy()).grid(row = 2,
+    checkFile = ttk.Button(text = "Send Files:", command = addedFunc).grid(row = 2,
                            column = 2,  padx = 10, pady = 10)
     timeLabel = ttk.Label(text = "Last File Check:" + str(cTime)).grid(row = 2,
                           column = 1, padx = 10, pady = 10)
@@ -56,7 +56,7 @@ def askDestination():
 def fileCopy():
     now = dt.datetime.now()
     c.execute('INSERT INTO fileTimes VALUES(?)',
-              (now.strftime('%Y-%M-%d %H: %M'),))
+              (now.strftime('%Y-%m-%d %H: %M'),))
     conn.commit()
 
  #get the paths for send and receive folders
@@ -71,12 +71,15 @@ def fileCopy():
             shutil.copy2(path, receiving)
             print (newFiles)
 
+def fileTime():
 #last file time
-c.execute('SELECT time FROM fileTimes ORDER by time DESC')
-cTime = c.fetchone()[0]
+   c.execute('SELECT time FROM fileTimes ORDER by time DESC')
+   cTime = c.fetchone()[0]
 
             
-
+def addedFunc():
+    fileCopy()
+    fileTime()
 
 
     
