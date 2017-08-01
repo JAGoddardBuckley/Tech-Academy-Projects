38b18e0afc8d8c92b6a4404d3c91e726d92206fd
Jennifer Goddard Buckley <jgbcodes@gmail.com>
2017-08-01 13:25:53 -0700

    ChallengeConditonalRadioButton
    Radio Button and image challenge  using Visual Studio for C# course at the Tech Academy

---
+++ CahllengeConditionalRadio/CahllengeConditionalRadio/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CahllengeConditionalRadio.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Your Note Taking Preferences<br />
+            <br />
+            <asp:RadioButton ID="PencilButton" runat="server" GroupName="NoteTakers"  Text="Pencil" />
+            <br />
+            <asp:RadioButton ID="PenButton" runat="server" GroupName="NoteTakers"  Text="Pen" />
+            <br />
+            <asp:RadioButton ID="PhoneButton" runat="server" GroupName="NoteTakers" Text="Phone" />
+            <br />
+            <asp:RadioButton ID="TabletButton" runat="server" GroupName="NoteTakers"  Text="Tablet" />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Height="26px" OnClick="Button1_Click" Text="OK" Width="33px" />
+            <br />
+            <asp:Image ID="resultImage" runat="server" ImageAlign="Middle" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ CahllengeConditionalRadio/CahllengeConditionalRadio/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CahllengeConditionalRadio
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (PencilButton.Checked)
+                resultLabel.Text = "You selected Pencil";
+            else if (PenButton.Checked)
+                resultLabel.Text = "You selected Pen";
+            else if (PhoneButton.Checked)
+                resultLabel.Text = "You selected Phone";
+            else if (TabletButton.Checked)
+                resultLabel.Text = "You selected Tablet";
+            else
+                resultLabel.Text = "Please select an option";
+
+
+            if (PencilButton.Checked)
+                resultImage.ImageUrl = "pencil.png";
+            else if (PenButton.Checked)
+                resultImage.ImageUrl = "pen.png";
+            else if (PhoneButton.Checked)
+                resultImage.ImageUrl = "phone.png";
+            else if (TabletButton.Checked)
+                resultImage.ImageUrl = "tablet.png";
+        }
+
+                
+    }
+}
